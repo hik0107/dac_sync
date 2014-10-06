c15e8d792e1de1e96c14267da485b33b22e08e2f
hik0107 <hkashida0107@gmail.com>
2014-10-06 20:34:52 +0900

    test3

---
+++ sql/fsp_hundling.sql
@@ -0,0 +1,66 @@
+﻿/*/ 
+FSP data hundling
+/*/ 
+
+
+
+select count(column_no) from info2_raw_fsp_data
+
+
+select "年月",count("年月") 
+ from raw_fsp_data
+ group by "年月"
+ order by "年月"
+
+/*/ 201307 ~ 201406  /*/ 
+/*/ 201307 ~ 201312 : discounting   /*/ 
+/*/ 201307 ~ 201312 : points  /*/ 
+
+select "年月",count("年月") 
+ from raw_fsp_data
+ where "年月" >= 201401
+ group by "年月"
+
+
+
+ select "店舗",count("店舗") 
+ from raw_fsp_data
+ group by "店舗"
+ order by  count("店舗")  ;
+
+ select "店舗",count("店舗") 
+ from raw_fsp_data
+where "店舗" = 's9111081'
+group by "店舗"
+ 
+/*/ 10 stores exit : # of data varies from 375K to 2.6M /*/
+
+
+ create table fsp_englishsub as
+ (select 
+   "年月" as "yyyymm",
+  "店舗" as "store",
+  "顧客CD" as "customer_code",
+  "POS中分類CD" as "category_code",
+  "POS中分類名" as "category",
+  "セグメントID" as "segment_code",
+  "セグメント名" as "segment",
+  "本部推奨売価" as "sales_price",
+  "特典値" as "tokuten",
+  "売価差" as "price_gap",
+  "値引額" "discounting",
+  "FLG" as "flg",
+  "購入FLG" as "purchase_flg",
+  "権利適用FLG" as "execute_flg",
+  "権利数" as "qty_own",
+  "FSP数量" as "qty_fsp",
+  "特売数量" as "qty_tokubai",
+  "定番数量" as "qty_teiban",
+  "月間数量" as "qty_gekkan"
+ 
+ from raw_fsp_data
+ )
+
+drop table fsp_englishsub
+
+/*/  where ("年月" >= 201401) AND  ("店舗"='s9111081') ; /*/
---
+++ sql/receipt_hundling.sql
@@ -0,0 +1,60 @@
+﻿/*/
+ recipt data /*/
+
+
+ select count(*) from raw_receipt
+/*/# of records ; 18,626,993 /*/
+
+ select substr("システム年月日",1,6),count(*) 
+ from raw_receipt
+ group by substr("システム年月日",1,6)
+ order by substr("システム年月日",1,6)
+
+ /*/+++++++++++++++++++/*/
+/*/ try to focus on store 's9111081' /*/
+select  "店舗",count(*) 
+from raw_receipt
+group by  "店舗" ;
+/*/ 558,940 records in caes of one focused store /*/
+
+select  "店舗",count(*) 
+from raw_receipt
+where "店舗" =  's9111081' and substr("システム年月日",1,6) >=201401
+group by  "店舗";
+/*/ 277,176 records in this compaction /*/
+
+
+
+
+
+ /*/+++++++++++++++++++/*/
+/*/ create table ---------------------- /*/
+drop table receipt_englishsub ;
+
+create table receipt_englishsub 
+as 
+(
+select
+  "システム年月日" as "yyyymmdd",
+  substr("システム年月日",1,6) as "yyyymm",
+  "販売時刻" as "time",
+  "店舗" as "store",
+  "顧客CD" as "customer_code",
+  "POS大分類CD" as "category1_code",
+  "POS大分類名" as "category1",
+  "POS中分類CD" as "category2_code",
+  "POS中分類名" as "category2",
+  "JANCD"  as "JAN_code",
+  "商品名" as "product",
+  "規格" as "kikaku",
+  "販売売上" as "qty_sales",
+  "数量" as "qty_unit",
+  "レジ番号" as "regi_number",
+  "レシート番号" as "receipt_number",
+  "シーケンス番号" as "seq_nmumber",
+  "FSP利用フラグ" as "fsp_execute_flag",
+  "セグメントID" as "segment"
+from raw_receipt
+)
+
+/*/ where "店舗" =  's9111081' and substr("システム年月日",1,6) >=201401 /*/
